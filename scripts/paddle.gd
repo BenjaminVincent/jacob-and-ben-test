3ca55ef43188f68aaa4552dbc3a7ade30a1c4fce
Jazeck1 <jzsaxophone@gmail.com>
2026-03-24 15:25:52 -0700

    added player movement

--- scripts/paddle.gd
+++ scripts/paddle.gd
@@ -10,7 +10,13 @@ func _ready() -> void:
 	
 	
 func _process(delta) -> void:
-	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var p1_dir = Input.get_vector("ui_left", "ui_right", "P1_UP", "P1_DOWN")
+	var p2_dir = Input.get_vector("ui_left", "ui_right", "P2_UP", "P2_DOWN")
 	
 	# Apply movement (for Node2D)
-	position += direction * speed * delta
+	if name == "P1": 
+		position.y += p1_dir.y * speed * delta
+	#print("direction is: ", direction)
+
+	if name == "P2": 
+		position.y += p2_dir.y * speed * delta
